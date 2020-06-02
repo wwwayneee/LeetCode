10884e0ae157ade833f0c444aa304ff9ace13516
Yuxuan Zhang <zhangyuxuan171@mails.ucas.ac.cn>
2020-06-02 18:15:15 +0800

    mac

---
+++ Explore/top_interview_questions_easy/27/27/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  27
+//
+//  Created by 张宇轩 on 2020/6/1.
+//  Copyright © 2020 张宇轩. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	printf("Hello, World!\n");
+	return 0;
+}
